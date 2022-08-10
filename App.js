2d34cce2b1a44141b2d9601453b7f8c976e57572
nshutidavid <nshutitoto@gmail.com>
2022-08-10 19:31:40 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <HomeScreen />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
